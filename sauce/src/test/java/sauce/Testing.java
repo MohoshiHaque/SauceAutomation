6f5e940ffcec8ba29d32b800bed89807a6647612
mohoshi.haque <mohoshihaque@gmail.com>
2023-08-08 12:34:40 +0600

    2nd commit

--- sauce/src/test/java/sauce/Testing.java
+++ sauce/src/test/java/sauce/Testing.java
@@ -1,5 +1,8 @@
 package sauce;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -17,10 +20,7 @@ public class Testing {
 		driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
-
-	    Thread.sleep(6000);
 	}
-	
 	
 	@Test(priority=1)
 	
@@ -42,6 +42,44 @@ public class Testing {
 	    Assert.assertEquals(Actualurl, Expectedurl,"Kmn");
 	}
 	
+	@Test(priority=3)
 	
+	public void InputInvalidUserName() throws InterruptedException {
+		
+		String NameExpectWarning = "Epic sadface: Username and password do not match any user in this service";
+		driver.findElement(By.id("user-name")).sendKeys("standard_user1");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		String NameActualWarning= driver.findElement(By.className("error-message-container")).getText();
+		System.out.println(NameActualWarning);
+		Assert.assertEquals(NameActualWarning,NameExpectWarning,"Pass");
+		
+		//Clear Name and password
+		driver.findElement(By.id("user-name")).clear();
+		driver.findElement(By.id("password")).clear();
+		
+	}
+	
+	
+	@Test(priority=4)
+	public void InputInvalidPassword() throws InterruptedException {
+		
 
+		String PassExpectWarning = "Epic sadface: Username and password do not match any user in this service";
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("secret_sauce1");
+		driver.findElement(By.id("login-button")).click();
+		
+		String PassActualWarning= driver.findElement(By.className("error-message-container")).getText();
+		System.out.println(PassActualWarning);
+		Assert.assertEquals(PassActualWarning,PassExpectWarning,"Pass");
+		
+		
+	}
+	
+	
+	
 }
